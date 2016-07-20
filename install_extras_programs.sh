e96f39d0afd4fbe7b8c6657577926e0a2de2e491
pedroaugusto <peadalmeida@gmail.com>
2016-07-19 23:08:26 -0300

    Update some files

---
+++ install_extras_programs.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y xubuntu-restricted-extras vlc gimp build-essential tlp tlp-rdw comix
+sudo tlp start
+
+echo "Your missing programs are installed!"
---
+++ install_java.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
+
+echo "This sucks"
+
+sudo apt-get install oracle-java8-installer -y
